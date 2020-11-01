7ff26e7e7e6f4d1aa71e1918c1f3d8d54b8478c5
chinhle <cdl180003@utdallas.edu>
2020-11-01 12:40:29 -0600

    Lecture126 error checking in middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from "express"
+import { DatabaseConnectionError } from "../errors/database-connection-err"
+import { RequestValidatonError } from '../errors/request-validation-err'
 
 export const errorHandle = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log("Something went wrong", err)
+    if (err instanceof RequestValidatonError) {
+        console.log("Handling this error as a request validation error")
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log("Handling this error as a db connection error")
+    }
 
     res.status(400).send({
         message: err.message
