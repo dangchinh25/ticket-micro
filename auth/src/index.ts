aa298ee8c90fb03c9910278db46fbcb3f02933f3
chinhle <cdl180003@utdallas.edu>
2020-10-31 20:42:49 -0500

    Lecture99 add skaffold config file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,4 @@ import {json} from "body-parser"
 const app = express()
 app.use(json())
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.listen(3000, () => console.log("Listening on port 3000!!!"))
