fd98c19eb33f62676cfdb49da7ec114b3de08175
chinhle <cdl180003@utdallas.edu>
2020-11-04 13:45:33 -0600

    Lecture158 create jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from "jsonwebtoken"
+
 import { User } from "../models/user"
 import { RequestValidatonError } from '../errors/request-validation-err'
 import { BadRequestError } from "../errors/bad-request-error"
@@ -33,6 +35,12 @@ router.post(
 
         const user = User.build({ email, password })
         await user.save()
+
+        // Generate JWT
+        const userJwt = jwt.sign({ id: user.id, email: user.email }, "1234")
+
+        // Store it on session object
+        req.session = { jwt: userJwt }
 
         res.status(201).send(user)
     }
