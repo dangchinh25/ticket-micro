b1c0419d49667c740bcbe9165a6fc126e94c8a77
chinhle <cdl180003@utdallas.edu>
2020-11-05 00:51:31 -0600

    Lecture165 sign in flow

--- auth/src/routes/siginin.ts
+++ auth/src/routes/siginin.ts
@@ -1,9 +1,27 @@
-import express from "express"
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+
+import { RequestValidatonError } from '../errors/request-validation-err'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router()
 
-router.post("/api/users/signin", (req, res) => {
-    res.send("Hi there")
-})
+router.post(
+	'/api/users/signin',
+	[
+		body('email').isEmail().withMessage('Email must be valid'),
+		body('password')
+			.trim()
+			.notEmpty()
+			.withMessage('You must supply a password'),
+	],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			throw new RequestValidatonError(errors.array())
+		}
+	}
+)
 
 export { router as signinRouter }
