812aebb803d0850c83376072ef035eadb3943ff7
chinhle <cdl180003@utdallas.edu>
2020-11-05 21:13:51 -0600

    Lecture186 more test for invlid input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -7,3 +7,21 @@ it('returns a 201 on succesful signup', async () => {
 		.send({ email: 'test@test.com', password: 'password' })
 		.expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({ email: 'fvsdfvsdvsd', password: 'password' })
+		.expect(400);
+});
+
+it('returns a 400 with an invalid pasword', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({ email: 'test@test.com', password: 'p' })
+		.expect(400);
+});
+
+it('returns a 400 with mising email and password', async () => {
+	return request(app).post('/api/users/signup').send({}).expect(400);
+});
