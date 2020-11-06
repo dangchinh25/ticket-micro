5c03520d7da1b9432c2bc413608771e00a27a7d5
chinhle <cdl180003@utdallas.edu>
2020-11-05 20:55:32 -0600

    Lecture183 setup test environment

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
+
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+});
+
+beforeEach(async () => {
+	// reset the test db for each test so we have clean data
+	const collections = await mongoose.connection.db.collections();
+
+	for (let connection of collections) {
+		await connection.deleteMany({});
+	}
+});
+
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
+});
