5381a74aac36cf3fdc2c1f63efbd4ee7143a8579
chinhle <cdl180003@utdallas.edu>
2020-11-05 22:00:21 -0600

    Lecture193 auth helper function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
 let mongo: any;
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signin(): Promise<string[]>;
+		}
+	}
+}
 
 beforeAll(async () => {
 	process.env.JWT_KEY = 'asdf';
@@ -28,3 +37,17 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({ email, password })
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+
+	return cookie;
+};
