1d0c35976101946a72a3980bb5d5b4346eeae7f3
chinhle <cdl180003@utdallas.edu>
2020-11-05 21:23:31 -0600

    Lecture188 changing node_env during test environment

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,35 +1,35 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/siginin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { errorHandle } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/siginin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandle } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
 
 app.use(
 	cookieSession({
 		signed: false,
-		secure: true,
+		secure: process.env.NODE_ENV !== 'test',
 	})
-)
+);
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.all('*', () => {
-	throw new NotFoundError()
-})
+	throw new NotFoundError();
+});
 
-app.use(errorHandle)
+app.use(errorHandle);
 
-export { app }
+export { app };
