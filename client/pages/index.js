d38719068483cb26dbe0653590f8fa87add9e911
chinhle <cdl180003@utdallas.edu>
2020-11-07 13:33:35 -0600

    Lecture219 landing page content

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,18 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-	console.log(currentUser);
-
-	return <h1>Landing page</h1>;
+	return currentUser ? (
+		<h1>You are signed in </h1>
+	) : (
+		<h1>You are not signed in </h1>
+	);
 };
 
 // when getInitailProps get called inside the server, one of the atributes passed to it is the "req", which is the same as the req object in ExpressJs
 LandingPage.getInitialProps = async (context) => {
 	const client = await buildClient(context);
 
-	const { data } = client.get('/api/users/currentuser');
+	const { data } = await client.get('/api/users/currentuser');
 
 	return data;
 };
