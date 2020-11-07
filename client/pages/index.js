2b6a2e5996de37b500b6da55dd756fc31f013b0e
chinhle <cdl180003@utdallas.edu>
2020-11-07 13:31:43 -0600

    Lecture218 refractor

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
 	console.log(currentUser);
@@ -7,24 +7,12 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // when getInitailProps get called inside the server, one of the atributes passed to it is the "req", which is the same as the req object in ExpressJs
-LandingPage.getInitialProps = async ({ req }) => {
-	if (typeof window === 'undefined') {
-		// we are on the server
-		// request should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local/
-		// SERVICENAME: kubectl get services
-		// NAMESPACE: kubectl get namespace
-		const { data } = await axios.get(
-			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-			{ headers: req.headers } // this is needed to inform ingress-nginx to recognise this use 'ticketing.dev' domain
-		);
+LandingPage.getInitialProps = async (context) => {
+	const client = await buildClient(context);
 
-		return data;
-	} else {
-		// we are on the browser
-		// request can be made using defaul domain name
-		const { data } = await axios.get('/api/users/currentuser');
-		return data;
-	}
+	const { data } = client.get('/api/users/currentuser');
+
+	return data;
 };
 
 export default LandingPage;
