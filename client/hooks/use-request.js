089ec26ecad1ec31adcba4426546a29ca05e7dcd
chinhle <cdl180003@utdallas.edu>
2020-11-07 11:59:25 -0600

    Lecture208 onSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
 	const [errors, setErrors] = useState(null);
 
 	const doRequest = async () => {
 		try {
 			setErrors(null);
-
 			const response = await axios[method](url, body);
+
+			if (onSuccess) {
+				onSuccess(response.data);
+			}
+
 			return response.data;
 		} catch (err) {
 			setErrors(
@@ -16,7 +20,7 @@ export default ({ url, method, body }) => {
 					<h4>Ooops....</h4>
 					<ul className='my-0'>
 						{err.response.data.errors.map((err) => (
-							<li key={err.meesage}>{err.message}</li>
+							<li key={err.message}>{err.message}</li>
 						))}
 					</ul>
 				</div>
