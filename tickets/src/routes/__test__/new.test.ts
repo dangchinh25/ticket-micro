bd85c720beed6a9c79c1941ad85c1fd112a9aed8
DangChinh <dangchinh2501@gmail.com>
2020-11-09 18:49:59 -0600

    Lecture244 add test file

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listing to /api/tickets for post request', async () => {});
+it('can only be access if the user is signed in', async () => {});
+it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid price is provided', async () => {});
+it('create a ticket with valid input', async () => {});
