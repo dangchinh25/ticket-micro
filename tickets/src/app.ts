ec4f43611b00932b5fb6d64ee39ea0691a4133cc
DangChinh <dangchinh2501@gmail.com>
2020-11-09 19:18:02 -0600

    Lecture248 fake login during test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandle, NotFoundError } from '@clticketmicro/common';
+import { errorHandle, NotFoundError, currentUser } from '@clticketmicro/common';
 
 import { createTicketRouter } from './routes/new';
 
@@ -16,6 +16,8 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test',
 	})
 );
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -6,7 +6,19 @@ it('has a route handler listing to /api/tickets for post request', async () => {
 
 	expect(response.status).not.toEqual(404);
 });
-it('can only be access if the user is signed in', async () => {});
+
+it('can only be access if the user is signed in', async () => {
+	await request(app).post('/api/tickets').send({}).expect(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({});
+	expect(response.status).not.toEqual(401);
+});
+
 it('returns an error if an invalid title is provided', async () => {});
 it('returns an error if an invalid price is provided', async () => {});
 it('create a ticket with valid input', async () => {});
